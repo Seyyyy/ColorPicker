487258c30b212dd6b3f59f795b6011a4be9c3f30
Seyyyy <mysdc7@gmail.com>
2020-05-13 10:27:45 +0900

    add path

--- src/__init__.py
+++ src/__init__.py
@@ -8,5 +8,10 @@ def hello():
     name = "Hello World"
     return name
 
+@app.route('/good')
+def good():
+    name = "Hello World"
+    return name
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
