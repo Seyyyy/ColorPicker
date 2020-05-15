591f359f71b8f293c6b4989f334c3baeda001b32
Seyyyy <mysdc7@gmail.com>
2020-05-15 19:22:43 +0900

    test: add getColorFeature test

--- src/__colorPicker.test.py
+++ src/__colorPicker.test.py
@@ -19,12 +19,41 @@ class ColorPickerTest(unittest.TestCase):
         self.assertEqual(colorPicker.abstraction(testcase).tolist(), expected.tolist())
 
 
-    # def test_getColorFeature(self):
-    #     testcase = np.array([[255,255,255],[0,0,0],[255,255,255],[0,0,0]])
-    #     testcase = testcase.T
-    #     expected = np.array([[255,255,255,255],[255,255,255,255],[255,255,255,255]])
-    #     expected = expected.T
-    #     self.assertEqual(colorPicker.getColorFeature(testcase), expected.tolist())
+    def test_getColorFeature(self):
+        # abstraction()で値は11以下になっているようにする
+        testcase = np.array([[11,11,11],[0,0,0],[11,11,11],[0,0,0]])
+        # [hue, saturation, value]の配列に変換するために転置する
+        testcase = testcase.T
+        testcase = colorPicker.getColorFeature(testcase)
+        # getColorFeature()の返り値の中身がNdarrayだからlistに変換する必要がある(要改善)
+        testcase = [testcase[0].tolist(),testcase[1].tolist(),testcase[2].tolist()]
+        expected = [
+            [0.5, 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0.5],
+            [0.5, 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0.5],
+            [0.5, 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0.5]
+            ]
+        self.assertEqual(testcase,expected)
+
+
+    def test_colorRatio(self):
+        # 各要素の割合　どの色がどの程度使用されているかに使う
+        testcase = np.array([0, 12, 28, 3, 57])
+        expected = np.array([0, 0.12, 0.28, 0.03, 0.57])
+        self.assertEqual(colorPicker.colorRatio(testcase).tolist(), expected.tolist())
+
+
+    def test_fillZero(self):
+        # colorRatioが0~11の順番で並んでほしいので間を「0.0」で埋める
+        testcaseKind  = [0  ,1  ,2  ,3  ,4  ,5  ,6  ,7  ,8  ,10  ,11  ]
+        testcaseRatio = [0.2,0.1,0.5,0.3,0.7,0.6,0.8,0.9,0.4,0.11,0.12]
+        expected      = [0.2,0.1,0.5,0.3,0.7,0.6,0.8,0.9,0.4,0.0 ,0.11,0.12]
+        self.assertEqual(colorPicker.fillZero(testcaseKind, testcaseRatio).tolist(), expected)
+
+
+    def test_getEntropy(self):
+        testcase = np.array([0.5, 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0.5])
+        expected = 1
+        self.assertEqual(colorPicker.getEntropy(testcase), expected)
 
 
     def test_hueTojson(self):
